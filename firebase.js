7a8778ecfca503056f8e621e1af9acf427048cad
Kaushik Banerjee <kaushikbanerjeeportfolio@gmail.com>
2021-06-14 19:13:43 -0700

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCm6khyShP745H5JTbZVWjgEB8CPb1mKBQ",
+    authDomain: "clone-741c7.firebaseapp.com",
+    projectId: "clone-741c7",
+    storageBucket: "clone-741c7.appspot.com",
+    messagingSenderId: "121862374053",
+    appId: "1:121862374053:web:b3ebd56ac4f7648420034c"
+  };
